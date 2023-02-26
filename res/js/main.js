8409ca6bc2f1f03d46fe48303f9c70fd0e78bd77
adam <adam.erben@seznam.cz>
2023-02-26 10:08:55 +0100

    Init

--- res/js/main.js
+++ res/js/main.js
@@ -1,47 +1,59 @@
-const holes = document.querySelectorAll('.hole');
-  const scoreBoard = document.querySelector('.score');
-  const moles = document.querySelectorAll('.mole');
-  let lastHole;
-  let timeUp = false;
-  let score = 0;
+const holes = document.getElementsByClassName("hole");
+const start = document.getElementById("startGame");
+const scoreBoard = document.getElementById("score");
+const moles = document.getElementsByClassName("mole");
 
-  function randomTime(min, max) { 
-    return Math.round(Math.random() * (max - min) + min);
+let lastHole;
+let timeUp = false;
+let score = 0;
+let ended = false;
+
+const randomNumber = (min, max) => {
+  return Math.round(Math.random() * (max - min) + min);
+};
+
+start.onclick = () => {
+  startGame();
+}
+
+function randomHole(holes) {
+  const idx = randomNumber(0, 8);
+  const hole = holes[idx];
+  if (hole === lastHole) {
+    return randomHole(holes);
   }
+  lastHole = hole;
+  return hole;
+}
 
-  function randomHole(holes) {
-    const idx = Math.floor(Math.random() * holes.length);
-    const hole = holes[idx];
-    if (hole === lastHole) {
-      return randomHole(holes);
-    }
-    lastHole = hole;
-    return hole;
-  }
+function peep() {
+  const time = 1000;
+  const hole = randomHole(holes);
+  hole.classList.add("up");
+  setTimeout(() => {
+    hole.classList.remove("up");
+    if (!timeUp) peep();
+  }, time);
+}
 
-  function peep() {
-    const time = randomTime(200, 1000);
-    const hole = randomHole(holes);
-    hole.classList.add('up');
-    setTimeout(() => {
-      hole.classList.remove('up');
-      if (!timeUp) peep();
-    }, time);
-  }
-
-  function startGame() {
-    scoreBoard.textContent = 0;
+function startGame() {
+  if (ended === false) {
+    ended = true;
+    scoreBoard.innerText = 0;
     timeUp = false;
     score = 0;
     peep();
-    setTimeout(() => timeUp = true, 10000)
+    setTimeout(() => {
+      timeUp = true;
+      ended = false;
+    }, 10000);
   }
+}
 
-  function whack(e) {
-    if(!e.isTrusted) return; 
-    score++;
-    this.parentNode.classList.remove('up');
-    scoreBoard.textContent = score;
-  }
+function whack() {
+  score++;
+  this.parentNode.classList.remove("up");
+  scoreBoard.innerText = score;
+}
 
-  moles.forEach(mole => mole.addEventListener('click', whack));
+[...moles].forEach((mole) => (mole.onclick = whack));
